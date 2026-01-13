a008572403d1e5d2f2a270ae3a329d7e99a63f6e
Tobias Lisbakken <tl@braum.tobman.no>
2026-01-13 21:54:42 +0100

    port option

--- src/Services/MetricHost.cs
+++ src/Services/MetricHost.cs
@@ -1,13 +1,22 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using Prometheus;
 
-class MetricHost(
-) : IHostedService {
+class MetricHost : IHostedService {
 	
-	private KestrelMetricServer _server = new KestrelMetricServer(port: 8088);
+  private readonly int _port;
+  private readonly ILogger _logger;
+	private readonly KestrelMetricServer _server;
+
+  public MetricHost(ILogger<MetricHost> logger, IConfiguration conf) {
+    _port = conf.GetValue<int>("ExportPort", 8088);
+    _server = new(port: _port);
+    _logger = logger;
+  }
 
 	
 	public Task StartAsync(CancellationToken ct) {
+    _logger.LogInformation("Export metrcs on {port}", _port);
 		_server.Start();
 		return Task.CompletedTask;
 	}
